da291ea48fe85249e2d4603632994994ee19a4c5
TimLundSE26 <TimLundSE26@users.noreply.github.com>
2017-12-14 17:00:37 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,8 +1,8 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
+import scraperwiki
+import lxml.html
 #
 # # Read in a page
 # html = scraperwiki.scrape("http://foo.com")
@@ -22,3 +22,35 @@
 # All that matters is that your final data is written to an SQLite database
 # called "data.sqlite" in the current working directory which has at least a table
 # called "data".
+
+import re
+# import scraperwiki
+import requests
+# from lxml.html.soupparser import fromstring
+from time import sleep
+
+month = "Oct 17"
+
+def search(mth):
+  	request_data = { "searchCriteria.parish": ALL, "searchCriteria.ward": ALL, "month": mth, "dateType": "DC_Validated" , "searchType": "Application" }
+
+	# <input type="radio" name="dateType" value="DC_Validated" checked="checked" id="dateValidated">
+    
+	sleep(2)
+	result = requests.post('http://public.oxford.gov.uk/online-applications/search.do?action=monthlyList', request_data)
+
+	if not result:
+		print "No result returned"
+		return
+	
+	result_dom = fromstring(result.content)
+  
+  print len(result_dom.xpath("body"))
+	print len(result_dom.xpath("//div"))
+  
+  results = result_dom.xpath("//ul[@id='searchresults']")
+  print len(results)
+
+
+search(month)
+
