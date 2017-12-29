2a35d002fe142e8388f67d22db57043cbbbe44c4
TimLundSE26 <TimLundSE26@users.noreply.github.com>
2017-12-29 20:52:47 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -5,29 +5,31 @@ from lxml.html.soupparser import fromstring
 from time import sleep
 
 month = "Oct 17"
+laurlroot = "http://planning.lewisham.gov.uk"
 
 def search(mth):
-#  	request_data = { "searchCriteria.parish": ALL, "searchCriteria.ward": ALL, "month": mth, "dateType": "DC_Validated" , "searchType": "Application" }
-
   	request_data = {"month": mth, "dateType": "DC_Validated" , "searchType": "Application" }
-
-	# <input type="radio" name="dateType" value="DC_Validated" checked="checked" id="dateValidated">
-    
+	
 	sleep(2)
-	result = requests.post('http://public.oxford.gov.uk/online-applications/search.do?action=monthlyList', request_data)
-
+	result = requests.post('http://planning.lewisham.gov.uk/online-applications/monthlyListResults.do?action=firstPage', request_data)
+	
 	if not result:
 		print "No result returned"
 		return
 	
-	result_dom = fromstring(result.content)
-  
-  	print len(result_dom.xpath("body"))
-	print len(result_dom.xpath("//div"))
-  
-  	results = result_dom.xpath("//ul[@id='searchresults']")
-  	print len(results)
+	result_dom = fromstring(result.content)  
+  	applications = result_dom.xpath("//li[@class='searchresult']")
+  	print len(applications)	
+	
+	for application in applications:
+		link = "".join(application.xpath('a/@href')).strip()
+		description = "".join(application.xpath('a/text()')).strip()
+		address = "".join(application.xpath('p[@class="address"]/text()')).strip()
+		meta = "".join(application.xpath('p[@class="metaInfo"]/text()')).strip()	
+		print link, address
+	
 
+
+print "Starting run"
 
 search(month)
-
